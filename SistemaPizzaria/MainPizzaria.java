5a46e0241fe0445d749f386840cba4c733ad6a17
João Victor Mendes <NightFace777@gmail.com>
2025-05-08 15:32:25 +0000

    clear

--- SistemaPizzaria/MainPizzaria.java
+++ SistemaPizzaria/MainPizzaria.java
@@ -1,13 +1,24 @@
 package SistemaPizzaria;
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class MainPizzaria{
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
 
         System.out.println("Digite o seu nome: \n");
-        String nome = ler.nextLine(); 
+        String nome = ler.nextLine();
 
         System.out.println("Digite a quantidade de pizzas:  ");
+        int quantidade = ler.nextInt();
+
+        System.out.println("Digite o preço da pizza: ");
+        double preco = ler.nextDouble();
+
+        ClassePizza pizza = new ClassePizza(); // Crie uma instância de ClassePizza com um nome mais significativo
+        pizza.Construtor(nome, quantidade, preco);
+
+        System.out.println(pizza.toString()); // Chama o método toString() e imprime o resultado
+
+        ler.close();
     }
 }
